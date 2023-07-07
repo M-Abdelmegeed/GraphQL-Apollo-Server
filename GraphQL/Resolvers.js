125a345b8558307d252e269e7a44958db30bb888
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2023-07-07 21:32:37 +0300

    Create and delete coments resolvers

--- GraphQL/Resolvers.js
+++ GraphQL/Resolvers.js
@@ -50,6 +50,42 @@ const resolvers = {
     },
   },
   Mutation: {
+    async createComment(_, { postId, body }, context) {
+      const user = checkAuth(context);
+      //Empty
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          body: "Comment body must not be empty",
+        });
+      }
+      const post = await Post.findById(postId);
+      if (post) {
+        // Unshift appends to the array at the top (beginning)
+        post.comments.unshift({
+          body: body,
+          username: user.username,
+          createdAt: new Date(),
+        });
+        await post.save();
+        return post;
+      } else throw new Error("Sorry, post not found");
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found!");
+      }
+    },
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
       console.log(user);
